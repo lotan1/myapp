eebb9c34a9c5673870acf1861798adfacf76f6cb
lotan1 <lotanro@gmail.com>
2013-12-05 00:26:49 +0200

    innitial 1

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,5 @@
+class GreetingsController < ApplicationController
+  def hello_olam
+	@"ahallan ahallan"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '3ba8e8d56c6783e486fc601773cc8094f36f0bd9ad15e2351f7b980e79c8b8c15c2959b604c0de845dc399a431dfaf2d8800493c291b0842439453750aa0cf97'
